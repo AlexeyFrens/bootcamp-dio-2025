45939605dcf7ff08efa397132f6fb85fa43d5031
AlexeyFrens <leiloka46@gmail.com>
2025-03-01 19:25:52 -0300

    Initial commit

---
+++ src/main/java/io/github/alexeyfrens/santanderdev2025/SantanderDev2025Application.java
@@ -0,0 +1,13 @@
+package io.github.alexeyfrens.santanderdev2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDev2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SantanderDev2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/alexeyfrens/santanderdev2025/SantanderDev2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alexeyfrens.santanderdev2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDev2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
